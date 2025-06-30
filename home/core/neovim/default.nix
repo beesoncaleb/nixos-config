47a4bc234fc005158cb8c81b39dddc5921156133
Caleb Beeson <caleb.f.beeson@gmail.com>
2025-06-30 00:16:03 -0500

    Added nvim config

--- home/core/neovim/default.nix
+++ home/core/neovim/default.nix
@@ -11,15 +11,46 @@
 
   programs.nixvim = {
     enable = true;
+    defaultEditor = true;
+
+    globals.mapleader = " ";
+    globalOpts = {
+      number = true;
+      relativenumber = true;
+      ls = 2;
+
+      wrap = false;
+      autoindent = true;
+      softtabstop = 2;
+      shiftwidth = 2;
+      expandtab = true;
+      
+      hlsearch = true;
+    };
+
+    keymaps = [
+      # Telescope binds
+      {
+        action = "<cmd>Telescope live_grep<CR>";
+	key = "<leader>fg";
+      }
+      {
+        action = "<cmd>Telescope find_files<CR>";
+	key = "<leader>ff";
+      }
+    ];
     
-    colorschemes.catppuccin = {
+    colorschemes.onedark = {
       enable = true;
-      settings.flavor = "mocha";
     };
 
     plugins = {
 
       treesitter = {
+        enable = true;
+      };
+
+      telescope = {
         enable = true;
       };
 
