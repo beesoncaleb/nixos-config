e5f74235639840c1f4b8da6a7ce5b59e533a1260
Caleb Beeson <caleb.f.beeson@gmail.com>
2025-07-15 17:39:37 -0500

    Added python 3.13

--- home/core/default.nix
+++ home/core/default.nix
@@ -23,6 +23,7 @@
     gcc
     wl-clipboard
     upower
+    python313
   ];
 
 }
--- hosts/nix-thinkpad/vm.nix
+++ hosts/nix-thinkpad/vm.nix
@@ -5,11 +5,24 @@
   ...
 }: {
 
-  users.users.${username}.extraGroups = [ "libvirtd" ];
+  users.users.${username}.extraGroups = [ "libvirtd" "kvm" ];
 
   programs.virt-manager.enable = true;
   
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+
+    qemu = {
+      package = pkgs.qemu_kvm;
+
+      ovmf = {
+        enable = true;
+        packages = [ pkgs.OVMFFull.fd ];
+      };
+
+      swtpm.enable = true;
+    };
+  };
 
   virtualisation.spiceUSBRedirection.enable = true;
 
