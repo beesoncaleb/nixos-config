b7cebbd3280093c8d6293a7ce85c26640cd919db
Caleb Beeson <caleb.f.beeson@gmail.com>
2025-08-18 19:52:57 -0500

    Setup vm declarations

--- hosts/nix-thinkpad/vm.nix
+++ hosts/nix-thinkpad/vm.nix
@@ -8,22 +8,17 @@
   users.users.${username}.extraGroups = [ "libvirtd" "kvm" ];
 
   programs.virt-manager.enable = true;
-  
   virtualisation.libvirtd = {
     enable = true;
-
     qemu = {
       package = pkgs.qemu_kvm;
-
       ovmf = {
         enable = true;
         packages = [ pkgs.OVMFFull.fd ];
       };
-
       swtpm.enable = true;
     };
   };
-
   virtualisation.spiceUSBRedirection.enable = true;
 
 }
