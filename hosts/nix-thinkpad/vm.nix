287f2da90133455c3f598f31078d20fc9d9ea145
Caleb Beeson <caleb.f.beeson@gmail.com>
2025-08-19 00:58:31 -0500

    Switched to quickemu to vm

--- hosts/nix-thinkpad/vm.nix
+++ hosts/nix-thinkpad/vm.nix
@@ -5,20 +5,14 @@
   ...
 }: {
 
-  users.users.${username}.extraGroups = [ "libvirtd" "kvm" ];
+  users.users.${username}.extraGroups = [ "input" "kvm" ];
 
-  programs.virt-manager.enable = true;
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      ovmf = {
-        enable = true;
-        packages = [ pkgs.OVMFFull.fd ];
-      };
-      swtpm.enable = true;
-    };
-  };
-  virtualisation.spiceUSBRedirection.enable = true;
-
+  environment.systemPackages = with pkgs; [
+    quickemu
+    swtpm
+    qemu_kvm
+    virt-viewer
+    spice-gtk
+    OVMF
+  ];
 }
