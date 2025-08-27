d95812ea8e3113fa5b8137681fa522ad105e3698
Caleb Beeson <caleb.f.beeson@gmail.com>
2025-08-27 17:54:28 -0500

    Set theme and vimMode for obsidian

--- home/modules/obsidian.nix
+++ home/modules/obsidian.nix
@@ -6,6 +6,16 @@
 
   programs.obsidian = {
     enable = true;
+    
+    defaultSettings = {
+      app = {
+        vimMode = true;
+      };
+
+      appearance = {
+        theme = "obsidian";
+      };
+    };
   };
 
 }
