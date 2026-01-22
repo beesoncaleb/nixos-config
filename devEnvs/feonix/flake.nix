af0d77b2c85eb2b7f67a05d1f853339cf3a2f074
beesoncaleb <caleb.f.beeson@gmail.com>
2026-01-22 04:06:46 -0600

    Added dev env for Feonix project

---
+++ devEnvs/feonix/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Development shell for Feonix EPICS project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, unstable, ... }: let 
+
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    prisma-pkgs = import unstable { inherit system; };
+
+  in {
+    devShells."${system}".default = pkgs.mkShell {
+      
+      packages = [
+        pkgs.pnpm
+        pkgs.nodejs_24
+        prisma-pkgs.prisma-engines_6
+      ];
+
+      shellHook = ''
+        # Modify prompt to give visual indicator of shell
+        export PS1="\n\[\e[32m\][Feonix]\[\e[0m\] $PS1"
+      '';
+    };
+  };
+}
