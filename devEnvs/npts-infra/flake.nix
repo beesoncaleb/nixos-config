2cedd4f2638c0d29ae86b608fa7cd28ffb793b00
beesoncaleb <caleb.f.beeson@gmail.com>
2026-07-15 21:24:30 -0500

    Added NPTS infra dev shell

--- devEnvs/npts-infra/flake.nix
+++ devEnvs/npts-infra/flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "Development shell for UTDesign Procurement EPICS project";
+  description = "Development shell for Managing Infrastructure at NPTS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }: let 
@@ -14,14 +14,13 @@
     devShells."${system}".default = pkgs.mkShell {
       
       packages = with pkgs; [
-        pnpm
-        nodejs_24
-        prisma-engines_6
+        opentofu
+        awscli2
       ];
 
       shellHook = ''
         # Modify prompt to give visual indicator of shell
-        export PS1="\n\[\e[32m\][UTD-Procurement]\[\e[0m\] $PS1"
+        export PS1="\n\[\e[32m\][NPTS Infra]\[\e[0m\] $PS1"
       '';
     };
   };
