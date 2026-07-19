af199275db0e4d451647d883ac66f948661020ec
beesoncaleb <caleb.f.beeson@gmail.com>
2026-07-19 15:32:11 -0500

    Renabled screenshotting

--- home/hyprland-desktop/hyprland/hyprland.nix
+++ home/hyprland-desktop/hyprland/hyprland.nix
@@ -218,9 +218,9 @@ bind = $mainMod, Z, exec, $kill
 bind = $mainMod, F, fullscreen,
 
 # Screenshot Binds
-# bind = $mainMod, PRINT, exec, hyprshot -m window -m active -o $screenshotdir  # screenshot window
-# bind = , PRINT, exec, hyprshot -m output -m eDP-1 -o $screenshotdir           # screenshot screen
-# bind = $shiftMod, PRINT, exec, hyprshot -m region -o $screenshotdir           # screenshot selected area
+bind = $mainMod, PRINT, exec, hyprshot -m window -m active -o $screenshotdir  # screenshot window
+bind = , PRINT, exec, hyprshot -m output -m eDP-1 -o $screenshotdir           # screenshot screen
+bind = $shiftMod, PRINT, exec, hyprshot -m region -o $screenshotdir           # screenshot selected area
 
 # Move focus with mainMod + arrow keys
 bind = $mainMod, H, movefocus, l
